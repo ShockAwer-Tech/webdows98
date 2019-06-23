ce8db34699ada445916b9513ae2010163ea0aef0
Leo Nesfield <thelmgn@Leos-MacBook-Pro.local>
2019-06-23 18:15:47 +0100

    scandisk

--- js/bios.js
+++ js/bios.js
@@ -9,10 +9,25 @@ var lines = `WebBios BIOS v1.00
 Copyright (C) 2019, theLMGN Technologies, Inc.
 
 GenuineIntel-Pentium(r) II Processor 266 MHz
-Memory Testing  : 524288K OK
+Memory Testing  : 65536 OK
 
 WebBIOS Plug and Play BIOS Extension  v1.0A
 Copyright (C) 2019, theLMGN Technologies, Inc.`
+
+function updateScandisk(pcnt) {
+    var noPos = "░"
+    var yesChar = "█"
+    var bar = ""
+    pcnt = pcnt / 100
+    if (pcnt < 10) {
+        bar = yesChar.repeat(Math.ceil(pcnt * 68)) + noPos.repeat(68 - Math.floor(pcnt * 68))
+    } else {
+        bar = yesChar.repeat(Math.ceil(pcnt * 67)) + noPos.repeat(67 - Math.floor(pcnt * 67))
+    }
+
+    scandisk.querySelector(".alignbottom").innerHTML = `<hr>
+    ${Math.floor(pcnt * 100)}% complete ${bar}`
+}
 
 async function go() {
     bios.innerText = ""
@@ -30,5 +45,16 @@ async function go() {
     await sleep(1000)
     bios.style.display = "none"
     scandisk.style.display = "block"
+
+    for (x = 0; x < 61; x++) {
+        updateScandisk(x)
+        await sleep((Math.random() * 60) + 30)
+    }
+    await sleep(2500)
+    for (x = 60; x < 101; x++) {
+        updateScandisk(x)
+        await sleep((Math.random() * 60) + 30)
+    }
+    await sleep(1000)
 }
 go()
